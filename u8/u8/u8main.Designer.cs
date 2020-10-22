5a035b62226913ebe3f98368b40aee845910ffde
Your Name <you@example.com>
2020-10-22 15:13:39 +0800

    u8打包可视化工具--VS2019

---
+++ u8/u8/u8main.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace u8
+{
+    partial class u8main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.Button();
+            this.path_string = new System.Windows.Forms.TextBox();
+            this.game = new System.Windows.Forms.Label();
+            this.game_choose = new System.Windows.Forms.ComboBox();
+            this.channel = new System.Windows.Forms.Label();
+            this.channel_choose = new System.Windows.Forms.ComboBox();
+            this.run = new System.Windows.Forms.Button();
+            this.open_apk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(87, 34);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(195, 31);
+            this.path.TabIndex = 0;
+            this.path.Text = "选择脚本文件地址";
+            this.path.UseVisualStyleBackColor = true;
+            this.path.Click += new System.EventHandler(this.path_Click);
+            // 
+            // path_string
+            // 
+            this.path_string.Enabled = false;
+            this.path_string.Location = new System.Drawing.Point(35, 89);
+            this.path_string.Multiline = true;
+            this.path_string.Name = "path_string";
+            this.path_string.Size = new System.Drawing.Size(287, 30);
+            this.path_string.TabIndex = 1;
+            this.path_string.TextChanged += new System.EventHandler(this.path_string_TextChanged);
+            // 
+            // game
+            // 
+            this.game.AutoSize = true;
+            this.game.Location = new System.Drawing.Point(46, 156);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(53, 12);
+            this.game.TabIndex = 2;
+            this.game.Text = "游戏选择";
+            // 
+            // game_choose
+            // 
+            this.game_choose.FormattingEnabled = true;
+            this.game_choose.Location = new System.Drawing.Point(141, 153);
+            this.game_choose.Name = "game_choose";
+            this.game_choose.Size = new System.Drawing.Size(170, 20);
+            this.game_choose.TabIndex = 3;
+            this.game_choose.SelectedIndexChanged += new System.EventHandler(this.game_choose_SelectedIndexChanged);
+            // 
+            // channel
+            // 
+            this.channel.AutoSize = true;
+            this.channel.Location = new System.Drawing.Point(46, 202);
+            this.channel.Name = "channel";
+            this.channel.Size = new System.Drawing.Size(53, 12);
+            this.channel.TabIndex = 4;
+            this.channel.Text = "渠道选择";
+            // 
+            // channel_choose
+            // 
+            this.channel_choose.FormattingEnabled = true;
+            this.channel_choose.Location = new System.Drawing.Point(141, 199);
+            this.channel_choose.Name = "channel_choose";
+            this.channel_choose.Size = new System.Drawing.Size(170, 20);
+            this.channel_choose.TabIndex = 5;
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(35, 252);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(92, 34);
+            this.run.TabIndex = 6;
+            this.run.Text = "执行u8打包";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // open_apk
+            // 
+            this.open_apk.Location = new System.Drawing.Point(226, 252);
+            this.open_apk.Name = "open_apk";
+            this.open_apk.Size = new System.Drawing.Size(96, 34);
+            this.open_apk.TabIndex = 7;
+            this.open_apk.Text = "打开APK目录";
+            this.open_apk.UseVisualStyleBackColor = true;
+            this.open_apk.Click += new System.EventHandler(this.open_apk_Click);
+            // 
+            // u8main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 318);
+            this.Controls.Add(this.open_apk);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.channel_choose);
+            this.Controls.Add(this.channel);
+            this.Controls.Add(this.game_choose);
+            this.Controls.Add(this.game);
+            this.Controls.Add(this.path_string);
+            this.Controls.Add(this.path);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "u8main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "u8打包工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button path;
+        private System.Windows.Forms.TextBox path_string;
+        private System.Windows.Forms.Label game;
+        private System.Windows.Forms.ComboBox game_choose;
+        private System.Windows.Forms.Label channel;
+        private System.Windows.Forms.ComboBox channel_choose;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button open_apk;
+    }
+}
+
---
+++ u8/u8/u8main.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+using System.IO;
+
+namespace u8
+{
+   
+    public partial class u8main : Form
+    {
+
+        string xml = Application.StartupPath + "/channel.xml";
+        string xmlNode = "/games";
+
+        public u8main()
+        {
+            InitializeComponent();
+
+            game_choose.Items.Clear();
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xml);
+            //获取指定节点
+            XmlNode rootNode = doc.SelectSingleNode(xmlNode);
+            //在指定节点中查找节点
+            foreach (XmlNode node in rootNode.ChildNodes)
+            {
+                if (node.Name == "path")
+                {
+                    //获取"path"节点中"name"的值
+                    string path = node.Attributes["name"].Value;
+                    path_string.Text = path;
+                }
+             
+                if (node.Name == "game") 
+                {
+                    ComboboxItem cbb = new ComboboxItem();
+                    cbb.Name = node.Attributes["name"].Value;
+                    cbb.Id = node.Attributes["id"].Value;
+                    cbb.Key = node.Attributes["key"].Value;
+                    game_choose.Items.Add(cbb);          
+                }
+
+            }
+
+            game_choose.SelectedIndex = 0;
+        }
+
+        private void path_Click(object sender, EventArgs e)
+        {
+            //选择文件目录
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string file = fbd.SelectedPath;
+                path_string.Text = file;
+            }
+        }
+
+        private void path_string_TextChanged(object sender, EventArgs e)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xml);
+            XmlNode rootNode = doc.SelectSingleNode(xmlNode);
+
+            //XmlElement game = doc.CreateElement("game");//创建
+            //game.SetAttribute("name", "");
+            //XmlElement channel = doc.CreateElement("channel");
+            //channel.SetAttribute("name", "");
+            //game.AppendChild(channel);
+            //rootNode.AppendChild(game);
+            //doc.Save(xml);
+
+
+            foreach (XmlNode node in rootNode.ChildNodes)
+            {
+                if (node.Name == "path")
+                {
+                    //修改"path"节点中"name"的值
+                    XmlElement element = (XmlElement)node;
+                    element.SetAttribute("name", path_string.Text);                    
+                }
+
+                //if (node.Name == "game")
+                //{
+                //    if (node.Attributes["id"].Value.Equals("3"))
+                //    {
+                //        XmlElement el = (XmlElement)node;
+                //        el.RemoveAll();//删除
+                //    }
+
+                //    XmlNodeList nodeList = node.ChildNodes;
+                //    foreach (XmlNode childNode in nodeList)
+                //    {
+                //        if (childNode.Attributes["id"].Value == "anzhi")
+                //        {
+                //            XmlElement el = (XmlElement)node;
+                //            XmlElement childEl = (XmlElement)childNode;
+                //            el.RemoveChild(childEl);
+                //        }
+                //    }
+                //}
+
+            }
+            doc.Save(xml);
+        }
+
+        private void game_choose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            channel_choose.Items.Clear();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xml);
+            //获取指定节点
+            XmlNode rootNode = doc.SelectSingleNode(xmlNode);
+            //在节点中查找节点
+            foreach (XmlNode node in rootNode.ChildNodes)
+            {
+                if (node.Name == "game")
+                {
+                    string key = (game_choose.SelectedItem as ComboboxItem).Key.ToString();
+
+                    if (node.Attributes["key"].Value.Equals(key))
+                    {                     
+                        foreach (XmlNode childNode in node)
+                        {
+                            ComboboxItem cbb = new ComboboxItem();
+                            cbb.Name = childNode.Attributes["name"].Value;
+                            cbb.Id = childNode.Attributes["id"].Value;
+                            channel_choose.Items.Add(cbb);
+                        }                       
+                    }                  
+                }
+            }
+
+            channel_choose.SelectedIndex = 0;
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            string gamename = (game_choose.SelectedItem as ComboboxItem).Name.ToString();
+            string channelname = (channel_choose.SelectedItem as ComboboxItem).Name.ToString();
+
+            DialogResult dr = MessageBox.Show("已选择游戏 '" + gamename + "',渠道'" + channelname + "'",
+                "u8打包确认提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (dr == DialogResult.OK)
+            {   
+                string appname = (game_choose.SelectedItem as ComboboxItem).Key.ToString();
+                string channelid = (channel_choose.SelectedItem as ComboboxItem).Id.ToString();
+                
+                Process process = new Process();
+                process.StartInfo.WorkingDirectory = path_string.Text;//执行文件所在目录
+                process.StartInfo.FileName = "package.bat";//执行文件名
+
+                string batpath = path_string.Text + @"\package.bat";
+                Console.WriteLine(batpath);
+                if (!File.Exists(batpath)) {
+                    MessageBox.Show("脚本文件不存在，请重新选择路径！");
+                    return;
+                }
+          
+                process.StartInfo.Arguments = string.Format("-g {0} -c {1}", appname, channelid);//参数
+                //process.StartInfo.CreateNoWindow = true;
+                //process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;//这里设置DOS窗口不显示，经实践可行
+                process.Start();
+                //process.WaitForExit(); //运行时界面锁死,等待退出
+                process.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void open_apk_Click(object sender, EventArgs e)
+        {
+            string gameKey = (game_choose.SelectedItem as ComboboxItem).Key.ToString();
+            string channelId = (channel_choose.SelectedItem as ComboboxItem).Id.ToString();
+            string path = path_string.Text + @"\output\" + gameKey + @"\" + channelId;
+            Console.WriteLine(path);
+            if (!Directory.Exists(path)) {
+                MessageBox.Show("目录不存在，请先打包！");
+                return;
+            }
+            System.Diagnostics.Process.Start(path);
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Key { get; set; }
+
+        public ComboboxItem(){
+        }
+
+        //重写ComboBox的ToString方法，否则下拉框显示数据无法显示
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+}
